f61feaefe2118b980bc96fec82b635cc9192269c
ivanr <ivanr@DESKTOP-1TDC26E>
2023-06-03 14:38:33 +0200

    controladores y vistas del juego

--- CliAhorcado/src/main/java/com/mycompany/cliahorcado/DificultadController.java
+++ CliAhorcado/src/main/java/com/mycompany/cliahorcado/DificultadController.java
@@ -8,6 +8,41 @@ package com.mycompany.cliahorcado;
  *
  * @author ivanr
  */
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class DificultadController {
-    
+
+    @FXML
+    private Button btn_cancelar;
+
+    @FXML
+    private Button btn_fame;
+
+    @FXML
+    private Button btn_facil;
+
+    @FXML
+    private Button btn_normal;
+
+    @FXML
+    private Button btn_dificil;
+
+    @FXML
+    void switchToJuego(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToFame(ActionEvent event) throws IOException {
+        App.setRoot("hallOfFame");
+    }
+
 }
